66a27b0dc3148446efd782c77c1ec287cdd5b7c7
aadi1737 <aadi1737ug@gmail.com>
2025-09-15 23:30:08 +0530

    New

--- src/main/java/com/INGRES/INGRES/Config/WebConfig.java
+++ src/main/java/com/INGRES/INGRES/Config/WebConfig.java
@@ -6,12 +6,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    public WebConfig() {
+        System.out.println("WebConfig loaded!"); // Check Render logs for this
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        System.out.println("CORS Mappings are being configured!"); // This will show in Render logs
+        System.out.println("CORS Mappings are being configured!");
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // ADD OPTIONS
+                .allowedHeaders("*")
+                .allowCredentials(false); // Set to false when using "*" origin
     }
 }
