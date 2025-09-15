cc232dcab724a7ab2c7b8d87adec9ea3e9a58f38
aadi1737 <aadi1737ug@gmail.com>
2025-09-15 23:03:37 +0530

    Include fiel for webconfig

---
+++ src/main/java/com/INGRES/INGRES/Config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.INGRES.INGRES.Config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        System.out.println("CORS Mappings are being configured!"); // This will show in Render logs
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
