52c992b6c4a35477302b47890bc89758f7e0627d
aadi1737 <aadi1737ug@gmail.com>
2025-09-15 19:37:27 +0530

    Forcing inclusion of target directory for Render deployment

--- src/main/java/com/INGRES/INGRES/DTOs/OutputResponseTemp.java
+++ src/main/java/com/INGRES/INGRES/DTOs/OutputResponseTemp.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 
 import java.util.Map;
 
-@Data
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -16,5 +16,19 @@ public class OutputResponseTemp {
     private String nl_response;
     private Map<String, Object> visualization_data; // kyunki {} aa raha h
 
+    public String getNl_response() {
+        return nl_response;
+    }
 
+    public void setNl_response(String nl_response) {
+        this.nl_response = nl_response;
+    }
+
+    public Map<String, Object> getVisualization_data() {
+        return visualization_data;
+    }
+
+    public void setVisualization_data(Map<String, Object> visualization_data) {
+        this.visualization_data = visualization_data;
+    }
 }
